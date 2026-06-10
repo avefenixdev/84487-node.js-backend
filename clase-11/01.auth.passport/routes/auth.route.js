1aa447d09b1f0d8382aa008e22b3fe48a2125611
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-10 19:39:17 -0300

    Empezando mover las rutas de server a un archivo independiente

---
+++ clase-11/01.auth.passport/routes/auth.route.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const routerAuth = express.Router()
+
+// ! ---------------------- Rutas Usuarios
+
+// ! Ruta donde recibo la info de logueo (correo, password)
+routerAuth.post('/login', (req, res) => {
+    res.send('login')
+})
+// ! Ruta donde recibo la info de register ( nombre, correo, password, confirm-password )
+routerAuth.post('/register', (req, res) => {
+    res.send('register')
+})
+// ! Ruta deslogueo de usuario
+routerAuth.get('/logout', (req, res) => {
+    res.send('logout')
+})
+
+export default routerAuth // funcionalidad principal de este módulo
